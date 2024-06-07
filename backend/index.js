8dd43b043ecf2c90eeb71b204fffb18e58b3f1eb
Snehal-Salvi <snehalsalvi099@gmail.com>
2024-06-07 23:59:53 +0530

    created controller and routes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import bodyParser from 'body-parser';
 import { connectToDb } from "./config/mongoose.js";
+import userRoutes from './routes/user.routes.js'
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get("/",(req,res) => {
+    res.send("App is working");
+});
+
+app.use("/api/user", userRoutes);
 
 app.listen(8000, () => {
   console.log("Server is listening on port 8000");
