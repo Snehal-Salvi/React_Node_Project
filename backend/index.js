5db690fd278f65b28941527e387120881041fc5d
Snehal-Salvi <snehalsalvi099@gmail.com>
2024-06-08 00:45:00 +0530

    created a new user from postman

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import bodyParser from 'body-parser';
 import { connectToDb } from "./config/mongoose.js";
-import userRoutes from './routes/user.routes.js'
+import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.get("/",(req,res) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(8000, () => {
   console.log("Server is listening on port 8000");
