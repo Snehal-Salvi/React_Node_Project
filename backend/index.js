4f2e8ee8676ac97c44b2e77e7e4606d2968b3b8e
Snehal-Salvi <snehalsalvi099@gmail.com>
2024-06-13 22:32:00 +0530

    created signin functionality

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
 import express from "express";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 import cors from "cors";
 import { connectToDb } from "./config/mongoose.js";
-import userRoutes from './routes/user.routes.js';
-import authRoutes from './routes/auth.routes.js';
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/",(req,res) => {
-    res.send("App is working");
+app.get("/", (req, res) => {
+  res.send("App is working");
 });
 
 app.use("/api/user", userRoutes);
@@ -20,14 +23,14 @@ app.use("/api/auth", authRoutes);
 app.listen(8000, () => {
   console.log("Server is listening on port 8000");
   connectToDb();
-}); 
+});
 
-app.use((err,req,res,next) =>{
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   res.status(statusCode).json({
     success: false,
     statusCode,
-    message
+    message,
   });
-})
+});
